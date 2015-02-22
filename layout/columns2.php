6763e28f31ea756e96739a84bd04e6f22fc36c94
jazinheira <jazinheira@github.com>
2015-02-22 17:05:54 -0500

    Update links on footer to saylor.org

--- layout/columns2.php
+++ layout/columns2.php
@@ -172,12 +172,12 @@ echo $OUTPUT->doctype() ?>
        
        <ul class="footer-nav">
 <li><a href="/">Home</a></li>
-<li><a href="/pathways">Courses</a></li>
-<li><a href="/saylor-difference">Saylor Difference</a></li>
-<li><a href="/about">About</a></li>
-<li><a href="/donate">Donate</a></li>
-<li><a href="/frequently-asked-questions">FAQ</a></li>
-<li><a href="/feedback">Contact</a></li>
+<li><a href="http://saylor.org/pathways">Courses</a></li>
+<li><a href="http://saylor.org/saylor-difference">Saylor Difference</a></li>
+<li><a href="http://saylor.org/about">About</a></li>
+<li><a href="http://saylor.org/donate">Donate</a></li>
+<li><a href="http://saylor.org/frequently-asked-questions">FAQ</a></li>
+<li><a href="http://saylor.org/contact">Contact</a></li>
 </ul>
        
        </div>
