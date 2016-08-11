69c6b782749484dca39609ee5c3079c0cf933cde
jazinheira <jazinheira@users.noreply.github.com>
2016-08-11 12:06:53 -0400

    Update footer-share on remaining layouts

--- layout/incourse.php
+++ layout/incourse.php
@@ -212,10 +212,11 @@ echo $OUTPUT->doctype() ?>
                 <a class="logo" style="background-image: url('<?php echo $OUTPUT->pix_url('logo2', 'theme')?>');" href="/"></a>
             </div>
             <div class="footer-share">
-                <a title="Follow us on Facebook" target="_blank" href="https://www.facebook.com/SaylorFoundation" class="fa fa-facebook"></a>
-                <a title="Follow us on Twitter" target="_blank" href="https://twitter.com/saylordotorg" class="fa fa-twitter"></a>
-                <a title="Follow us on Google Plus" target="_blank" href="https://plus.google.com/105426497265909285606/posts" class="fa fa-google-plus"></a>
-                <a title="Follow us on GitHub" target="_blank" href="https://github.com/saylordotorg" class="fa fa-github"></a>
+                <a title="Facebook | /SaylorFoundation" target="_blank" href="https://www.facebook.com/SaylorFoundation" class="fa fa-facebook fa-2x"></a>
+                <a title="Twitter | @saylordotorg" target="_blank" href="https://twitter.com/saylordotorg" class="fa fa-twitter fa-2x"></a>
+                <a title="Google+ | +SaylorAcademy" target="_blank" href="https://plus.google.com/105426497265909285606/posts" class="fa fa-google-plus fa-2x"></a>
+                <a title="Instagram | @saylordotorg" target="_blank" href="https://wwwinstagram.com/saylordotorg" class="fa fa-instagram fa-2x"></a>
+                <a title="GitHub | /saylordotorg" target="_blank" href="https://github.com/saylordotorg" class="fa fa-github fa-2x"></a>
             </div>
         </div>
         <div class="span4">
