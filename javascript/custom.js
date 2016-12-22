276628a2b9d3e9ad8834d6da4519e8fdc8bbad89
sharmi55 <rai.sharmistha@gmail.com>
2016-12-22 15:10:16 -0500

    test removing custom.js

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,14 +1,14 @@
-jQuery(document).ready(
-    function ($) {
+// jQuery(document).ready(
+//     function ($) {
 
-        var $pArr = $('.coursebox.panel2');
-        var pArrLen = $pArr.length;
-        var pPerDiv = 2;
-        for (var i = 0; i < pArrLen; i += pPerDiv) {
-            $pArr.filter(':eq(' + i + '),:lt(' + (i + pPerDiv) + '):gt(' + i + ')').wrapAll('<div class="row-fluid clearfix" />');
-        }
+//         var $pArr = $('.coursebox.panel2');
+//         var pArrLen = $pArr.length;
+//         var pPerDiv = 2;
+//         for (var i = 0; i < pArrLen; i += pPerDiv) {
+//             $pArr.filter(':eq(' + i + '),:lt(' + (i + pPerDiv) + '):gt(' + i + ')').wrapAll('<div class="row-fluid clearfix" />');
+//         }
 
         // $('#block-region-side-pre .block').eq(0).addClass('firstblock');
 
-    }
-);
+//     }
+// );
