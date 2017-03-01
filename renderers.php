50a24e305b3a930fa36f65a0d5d4326c3f987a8c
jazinheira <jazinheira@users.noreply.github.com>
2017-03-01 10:42:33 -0500

    Revert "Merge branch 'development'"
    This reverts commit 1c70ece61883af2fae49c034348249689c514328, reversing
    changes made to f5c6003c45a5d4de420cc820942ac6327aff53ab.

--- renderers.php
+++ renderers.php
@@ -465,7 +465,8 @@ class theme_saylor_core_renderer extends core_renderer
     }
 }
 
-class theme_saylor_block_course_overview_renderer extends block_course_overview_renderer {
+class theme_saylor_block_course_overview_renderer extends block_course_overview_renderer
+{
 
     /**
      * Construct contents of course_overview block
@@ -688,112 +689,3 @@ class theme_saylor_block_course_overview_renderer extends block_course_overview_
         return $html;
     }
 }
-
-class theme_saylor_core_course_renderer extends core_course_renderer
-{
-           // Change searchcriteria to only focus on courses from category 2.
-    protected function coursecat_courses(coursecat_helper $chelper, $courses, $totalcount = null) {
-        global $CFG;
-        if ($totalcount === null) {
-            $totalcount = count($courses);
-        }
-        if (!$totalcount) {
-            // Courses count is cached during courses retrieval.
-            return '';
-        }
-
-        if ($chelper->get_show_courses() == self::COURSECAT_SHOW_COURSES_AUTO) {
-            // In 'auto' course display mode we analyse if number of courses is more or less than $CFG->courseswithsummarieslimit.
-            if ($totalcount <= $CFG->courseswithsummarieslimit) {
-                $chelper->set_show_courses(self::COURSECAT_SHOW_COURSES_EXPANDED);
-            } else {
-                $chelper->set_show_courses(self::COURSECAT_SHOW_COURSES_COLLAPSED);
-            }
-        }
-
-        // Prepare content of paging bar if it is needed.
-        $paginationurl = $chelper->get_courses_display_option('paginationurl');
-        $paginationallowall = $chelper->get_courses_display_option('paginationallowall');
-        if ($totalcount > count($courses)) {
-            // there are more results that can fit on one page.
-            if ($paginationurl) {
-                // the option paginationurl was specified, display pagingbar.
-                $perpage = $chelper->get_courses_display_option('limit', $CFG->coursesperpage);
-                $page = $chelper->get_courses_display_option('offset') / $perpage;
-                $pagingbar = $this->paging_bar($totalcount, $page, $perpage,
-                        $paginationurl->out(false, array('perpage' => $perpage)));
-                if ($paginationallowall) {
-                    $pagingbar .= html_writer::tag('div', html_writer::link($paginationurl->out(false, array('perpage' => 'all')),
-                            get_string('showall', '', $totalcount)), array('class' => 'paging paging-showall'));
-                }
-            } else if ($viewmoreurl = $chelper->get_courses_display_option('viewmoreurl')) {
-                // the option for 'View more' link was specified, display more link.
-                $viewmoretext = $chelper->get_courses_display_option('viewmoretext', new lang_string('viewmore'));
-                $morelink = html_writer::tag('div', html_writer::link($viewmoreurl, $viewmoretext),
-                        array('class' => 'paging paging-morelink'));
-            }
-        } else if (($totalcount > $CFG->coursesperpage) && $paginationurl && $paginationallowall) {
-            // there are more than one page of results and we are in 'view all' mode, suggest to go back to paginated view mode.
-            $pagingbar = html_writer::tag('div', html_writer::link($paginationurl->out(false, array('perpage' => $CFG->coursesperpage)),
-                get_string('showperpage', '', $CFG->coursesperpage)), array('class' => 'paging paging-showperpage'));
-        }
-
-        // Display list of courses.
-        $attributes = $chelper->get_and_erase_attributes('courses');
-        $content = html_writer::start_tag('div', $attributes);
-
-        if (!empty($pagingbar)) {
-            $content .= $pagingbar;
-        }
-
-        $coursecount = 0;
-        // First, create whitelist of courses in cat 2.
-        $options['recursive'] = true;
-        $options['coursecontacts'] = false;
-        $options['summary'] = false;
-        $options['sort']['idnumber'] = 1;
-
-        $cat2courselist = coursecat::get(2)->get_courses($options);
-        // Check all courses and put those with id 2 in whitelist.
-        foreach ($cat2courselist as $cat2course) {
-            $id = $cat2course->__get('id');
-            $cat2courses[$id] = $id;
-        };
-
-        // Get list of courses and check if each course is in category 2.
-        foreach ($courses as $course) {
-            $courseisincat2 = false; // False = 0
-            $coursecount ++;
-            // Checking if course is in whitelist.
-            foreach ($cat2courses as $cat2course) {
-                if ($cat2course == $course->id) {
-                    $courseisincat2 = true;
-                    break;
-                }
-            }
-            if ($courseisincat2 == false) {
-                continue;
-            }
-
-            $classes = ($coursecount % 2) ? 'odd' : 'even';
-            if ($coursecount == 1) {
-                $classes .= ' first';
-            }
-            if ($coursecount >= count($courses)) {
-                $classes .= ' last';
-            }
-            $content .= $this->coursecat_coursebox($chelper, $course, $classes);
-        }
-
-        if (!empty($pagingbar)) {
-            $content .= $pagingbar;
-        }
-        if (!empty($morelink)) {
-            $content .= $morelink;
-        }
-
-        $content .= html_writer::end_tag('div'); // .courses
-        return $content;
-    }
-}
-
