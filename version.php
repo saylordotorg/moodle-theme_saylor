7555563bc1023b56d7bb239b216e99d17d71b1fd
jazinheira <jazinheira@users.noreply.github.com>
2018-09-04 16:00:18 +0100

    Version bump (2017061000)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'theme_saylor';
-$plugin->version   = 2017060700;
+$plugin->version   = 2017061000;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->requires  = 2018051300;
 $plugin->dependencies = ['theme_boost' => '2018051300'];
