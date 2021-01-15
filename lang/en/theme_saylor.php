c1bbcdbb1e73d86609ba37079566d8197b3e444f
David Ta <david.ta@saylor.org>
2021-01-15 14:14:15 -0600

    Initial theme update
    Updated theme to make it compatible with Moodle 3.10. A few changes were made. Some stying convention has been updated. Renderer moved into the class folder. SASS files have been reorganized. Layout and template files have been updated.

--- lang/en/theme_saylor.php
+++ lang/en/theme_saylor.php
@@ -15,34 +15,45 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'theme_saylor', language 'en'
+ * Language file.
  *
  * @package   theme_saylor
- * @copyright 2018 Saylor Academy
+ * @copyright 2021 Saylor Academy
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// Check the file is being called internally from within Moodle.
 defined('MOODLE_INTERNAL') || die();
-
-$string['pluginname'] = 'Saylor';
 
 $string['advancedsettings'] = 'Advanced settings';
 $string['bannerdescription'] = 'Build new skills or work toward a degree at your own pace with free Saylor Academy college courses.';
-$string['choosereadme'] = 'Theme Saylor is a child theme of Boost.';                
-$string['configtitle'] = 'Saylor theme settings';
+$string['backgroundimage'] = 'Background image';
+$string['backgroundimage_desc'] = 'The image to display as a background of the site. The background image you upload here will override the background image in your theme preset files.';
+$string['brandcolor'] = 'Brand colour';
+$string['brandcolor_desc'] = 'The accent colour.';
+$string['bootswatch'] = 'Bootswatch';
+$string['bootswatch_desc'] = 'A bootswatch is a set of Bootstrap variables and css to style Bootstrap';
+$string['choosereadme'] = 'Saylor is a modern highly-customisable theme. This theme is intended to be used directly, or as a parent theme when creating new themes utilising Bootstrap 4.';
+$string['currentinparentheses'] = '(current)';
 $string['completed'] = 'Completed';
+$string['configtitle'] = 'Saylor theme settings';
 $string['generalsettings'] = 'General settings';
 $string['loginorsignupmessage'] = '<a href="{$a}"> Log in or Sign up</a> to track your course progress, gain access to final exams, and get a free certificate of completion!';
+$string['nobootswatch'] = 'None';
+$string['pluginname'] = 'Saylor';
 $string['presetfiles'] = 'Additional theme preset files';
-$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme.';
 $string['preset'] = 'Theme preset';
 $string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+$string['privacy:metadata'] = 'The Saylor theme does not store any personal data about any user.';
 $string['rawscss'] = 'Raw SCSS';
 $string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
 $string['rawscsspre'] = 'Raw initial SCSS';
 $string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
 $string['region-side-pre'] = 'Right';
+$string['privacy:metadata:preference:draweropennav'] = 'The user\'s preference for hiding or showing the drawer menu navigation.';
+$string['privacy:drawernavclosed'] = 'The current preference for the navigation drawer is closed.';
+$string['privacy:drawernavopen'] = 'The current preference for the navigation drawer is open.';
+$string['totop'] = 'Go to top';
 $string['signupidps'] = 'Create a new account using:';
 $string['logintext'] = 'Log in or Sign up';
 $string['loggedingreeting'] = 'Hi, {$a}!';
--- settings.php
+++ settings.php
@@ -15,52 +15,83 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- *
  * @package   theme_saylor
- * @copyright 2018 Saylor Academy
+ * @copyright 2021 Saylor Academy
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
+defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
-    // Boost provides a nice setting page which splits settings onto separate tabs. We want to use it here.                         
-    $settings = new theme_boost_admin_settingspage_tabs('themesettingsaylor', get_string('configtitle', 'theme_saylor'));             
- 
-    // Each page is a tab - the first is the "General" tab.                                                                         
-    $page = new admin_settingpage('theme_saylor_general', get_string('generalsettings', 'theme_saylor'));                             
- 
-    // Replicate the preset setting from boost.                                                                                     
-    $name = 'theme_saylor/preset';                                                                                                   
-    $title = get_string('preset', 'theme_saylor');                                                                                   
-    $description = get_string('preset_desc', 'theme_saylor');                                                                        
-    $default = 'default.scss';                                                                                                      
- 
-    // We list files in our own file area to add to the drop down. We will provide our own function to                              
-    // load all the presets from the correct paths.                                                                                 
-    $context = context_system::instance();                                                                                          
-    $fs = get_file_storage();                                                                                                       
-    $files = $fs->get_area_files($context->id, 'theme_saylor', 'preset', 0, 'itemid, filepath, filename', false);                    
- 
-    $choices = [];                                                                                                                  
-    foreach ($files as $file) {                                                                                                     
-        $choices[$file->get_filename()] = $file->get_filename();                                                                    
-    }                                                                                                                               
-    // These are the built in presets from Boost.                                                                                   
-    $choices['default.scss'] = 'default.scss';                                                                                      
-    $choices['plain.scss'] = 'plain.scss';                                                                                          
- 
-    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);                                     
-    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
-    $page->add($setting);                                                                                                           
- 
-    // Preset files setting.                                                                                                        
-    $name = 'theme_saylor/presetfiles';                                                                                              
-    $title = get_string('presetfiles','theme_saylor');                                                                               
-    $description = get_string('presetfiles_desc', 'theme_saylor');                                                                   
- 
-    $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0, array('maxfiles' => 20, 'accepted_types' => array('.scss')));
+    // $settings = new theme_saylor_admin_settingspage_tabs('themesettingsaylor', get_string('configtitle', 'theme_saylor'));
+    $settings = new theme_boost_admin_settingspage_tabs('themesettingsaylor', get_string('configtitle', 'theme_saylor'));
+    $page = new admin_settingpage('theme_saylor_general', get_string('generalsettings', 'theme_saylor'));
+
+    // Preset.
+    $name = 'theme_saylor/preset';
+    $title = get_string('preset', 'theme_saylor');
+    $description = get_string('preset_desc', 'theme_saylor');
+    $default = 'default.scss';
+
+    $context = context_system::instance();
+    $fs = get_file_storage();
+    $files = $fs->get_area_files($context->id, 'theme_saylor', 'preset', 0, 'itemid, filepath, filename', false);
+
+    $choices = [];
+    foreach ($files as $file) {
+        $choices[$file->get_filename()] = $file->get_filename();
+    }
+    // These are the built in presets.
+    $choices['default.scss'] = 'default.scss';
+    $choices['plain.scss'] = 'plain.scss';
+
+    $setting = new admin_setting_configthemepreset($name, $title, $description, $default, $choices, 'saylor');
+    $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
+
+    // Preset files setting.
+    $name = 'theme_saylor/presetfiles';
+    $title = get_string('presetfiles','theme_saylor');
+    $description = get_string('presetfiles_desc', 'theme_saylor');
+
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,
+        array('maxfiles' => 20, 'accepted_types' => array('.scss')));
+    $page->add($setting);
+
+    // Background image setting.
+    $name = 'theme_saylor/backgroundimage';
+    $title = get_string('backgroundimage', 'theme_saylor');
+    $description = get_string('backgroundimage_desc', 'theme_saylor');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'backgroundimage');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Variable $body-color.
+    // We use an empty default value because the default colour should come from the preset.
+    $name = 'theme_saylor/brandcolor';
+    $title = get_string('brandcolor', 'theme_saylor');
+    $description = get_string('brandcolor_desc', 'theme_saylor');
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, '');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
     // Must add the page after definiting all the settings!
-    $settings->add($page);                                                                                                       
+    $settings->add($page);
+
+    // Advanced settings.
+    $page = new admin_settingpage('theme_saylor_advanced', get_string('advancedsettings', 'theme_saylor'));
+
+    // Raw SCSS to include before the content.
+    $setting = new admin_setting_scsscode('theme_saylor/scsspre',
+        get_string('rawscsspre', 'theme_saylor'), get_string('rawscsspre_desc', 'theme_saylor'), '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Raw SCSS to include after the content.
+    $setting = new admin_setting_scsscode('theme_saylor/scss', get_string('rawscss', 'theme_saylor'),
+        get_string('rawscss_desc', 'theme_saylor'), '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    $settings->add($page);
 }
