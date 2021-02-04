e57e9a45e275db908dbeecf2db90d70e91959652
dta121 <74207428+dta121@users.noreply.github.com>
2021-02-04 15:19:06 -0600

    Initial theme update (#237)
    * Initial theme update
    
    Updated theme to make it compatible with Moodle 3.10. A few changes were made. Some stying convention has been updated. Renderer moved into the class folder. SASS files have been reorganized. Layout and template files have been updated.
    
    * New User Alerts in Settings (#238)
    
    Added a new feature that allows administrators to publish alerts that would be prominent on the frontpage and dashboard of the site.
    
    * Add radius to search boxes (#248)
    
    Co-authored-by: jazinheira <jazinheira@users.noreply.github.com>

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -14,34 +14,42 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace theme_saylor\output;
+
+use moodle_url;
+use html_writer;
+use context_course;
+use stdClass;
+use action_menu_filler;
+use action_menu;
+use pix_icon;
+use action_menu_link_secondary;
+
+defined('MOODLE_INTERNAL') || die;
+
 /**
  * Renderers to align Moodle's HTML with that expected by Bootstrap
  *
- * @package   theme_bootstrapbase
- * @copyright 2012
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    theme_saylor
+ * @copyright  2012 Bas Brands, www.basbrands.nl
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+class core_renderer extends \core_renderer {
 
-// Check the file is being called internally from within Moodle.
-defined('MOODLE_INTERNAL') || die();
+    public function edit_button(moodle_url $url) {
+        $url->param('sesskey', sesskey());
+        if ($this->page->user_is_editing()) {
+            $url->param('edit', 'off');
+            $editstring = get_string('turneditingoff');
+        } else {
+            $url->param('edit', 'on');
+            $editstring = get_string('turneditingon');
+        }
+        $button = new \single_button($url, $editstring, 'post', ['class' => 'btn btn-primary']);
+        return $this->render_single_button($button);
+    }
 
-// course renderer
-require_once($CFG->dirroot . "/theme/boost/classes/output/core_renderer.php");
-require_once($CFG->dirroot . "/course/renderer.php");
 
-class theme_saylor_core_renderer extends theme_boost\output\core_renderer
-{
-   /**
-     * Override the standard boost user menu construction in order to use some 
-     * custom greetings.
-     *
-     * Construct a user menu, returning HTML that can be echoed out by a
-     * layout file.
-     *
-     * @param stdClass $user A user object, usually $USER.
-     * @param bool $withlinks true if a dropdown should be built.
-     * @return string HTML fragment.
-     */
     public function user_menu($user = null, $withlinks = null) {
         global $USER, $CFG;
         require_once($CFG->dirroot . '/user/lib.php');
@@ -229,6 +237,7 @@ class theme_saylor_core_renderer extends theme_boost\output\core_renderer
             $usermenuclasses
         );
     }
+
     /*
     * This controls open graph meta property logic.
     */
@@ -247,7 +256,7 @@ class theme_saylor_core_renderer extends theme_boost\output\core_renderer
         // Show different info in courses, such as the title and images.
         if ($PAGE->pagelayout == 'course' || $PAGE->pagelayout == 'incourse') {
             $title = str_replace("Course: ", "", $PAGE->title)." | ".$SITE->shortname;
-            // Filter out instances where the course name is listed twice 
+            // Filter out instances where the course name is listed twice
             // where a resource has the course name in it (like the final exams).
             $title = str_replace($COURSE->shortname.": ".$COURSE->shortname.": ", $COURSE->shortname.": ", $title);
             $image = $COURSE->shortname."-1200x1200.png";
@@ -284,7 +293,7 @@ class theme_saylor_core_renderer extends theme_boost\output\core_renderer
         // Show different info in courses, such as the title and images.
         if ($PAGE->pagelayout == 'course' || $PAGE->pagelayout == 'incourse') {
             $title = str_replace("Course: ", "", $PAGE->title)." | ".$SITE->shortname;
-            // Filter out instances where the course name is listed twice 
+            // Filter out instances where the course name is listed twice
             // where a resource has the course name in it (like the final exams).
             $title = str_replace($COURSE->shortname.": ".$COURSE->shortname.": ", $COURSE->shortname.": ", $title);
             $image = $COURSE->shortname."-1200x1200.png";
@@ -347,110 +356,5 @@ class theme_saylor_core_renderer extends theme_boost\output\core_renderer
         $output .= html_writer::end_tag('div');
         return $output;
     }
-}
 
-class theme_saylor_core_course_renderer extends \core_course_renderer
-{
-           // Change searchcriteria to only focus on courses from category 2.
-    protected function coursecat_courses(coursecat_helper $chelper, $courses, $totalcount = null) {
-        global $CFG;
-        // New array with filtered courses.
-        $coursestorender = array();
-        // First, create whitelist of courses in cat 2.
-        $options['recursive'] = true;
-        $options['coursecontacts'] = false;
-        $options['summary'] = false;
-        $options['sort']['idnumber'] = 1;
-        $cat2courselist = core_course_category::get(2)->get_courses($options);
-        // Check all courses and put those with id 2 in whitelist.
-        foreach ($cat2courselist as $cat2course) {
-            $id = $cat2course->__get('id');
-            $cat2courses[$id] = $id;
-        }
-        // Get list of courses and check if each course is in category 2.
-        foreach ($courses as $course) {
-            $courseisincat2 = false; // False = 0
-            // Checking if course is in whitelist.
-            foreach ($cat2courses as $cat2course) {
-                if ($cat2course == $course->id) {
-                    $courseisincat2 = true;
-                    break;
-                }
-            }
-            // If you are an admin you can see everything otherwise you see only courses in cat 2.
-            if ($courseisincat2 == false && !is_siteadmin()) {
-                continue;
-            }
-            // Add filtered courses from whitelist into a new array.
-            $coursestorender[] = $course;
-        }
-        if ($totalcount === null) {
-            $totalcount = count($coursestorender);
-        }
-        if (!$totalcount) {
-            // Courses count is cached during courses retrieval.
-            return '';
-        }
-        if ($chelper->get_show_courses() == self::COURSECAT_SHOW_COURSES_AUTO) {
-            // In 'auto' course display mode we analyse if number of courses is more or less than $CFG->courseswithsummarieslimit.
-            if ($totalcount <= $CFG->courseswithsummarieslimit) {
-                $chelper->set_show_courses(self::COURSECAT_SHOW_COURSES_EXPANDED);
-            } else {
-                $chelper->set_show_courses(self::COURSECAT_SHOW_COURSES_COLLAPSED);
-            }
-        }
-        // Prepare content of paging bar if it is needed.
-        $paginationurl = $chelper->get_courses_display_option('paginationurl');
-        $paginationallowall = $chelper->get_courses_display_option('paginationallowall');
-        if ($totalcount > count($courses)) {
-            // There are more results that can fit on one page.
-            if ($paginationurl) {
-                // The option paginationurl was specified, display pagingbar.
-                $perpage = $chelper->get_courses_display_option('limit', $CFG->coursesperpage);
-                $page = $chelper->get_courses_display_option('offset') / $perpage;
-                $pagingbar = $this->paging_bar($totalcount, $page, $perpage,
-                        $paginationurl->out(false, array('perpage' => $perpage)));
-                if ($paginationallowall) {
-                    $pagingbar .= html_writer::tag('div', html_writer::link($paginationurl->out(false, array('perpage' => 'all')),
-                            get_string('showall', '', $totalcount)), array('class' => 'paging paging-showall'));
-                }
-            } else if ($viewmoreurl = $chelper->get_courses_display_option('viewmoreurl')) {
-                // The option for 'View more' link was specified, display more link.
-                $viewmoretext = $chelper->get_courses_display_option('viewmoretext', new lang_string('viewmore'));
-                $morelink = html_writer::tag('div', html_writer::link($viewmoreurl, $viewmoretext),
-                        array('class' => 'paging paging-morelink'));
-            }
-        } else if (($totalcount > $CFG->coursesperpage) && $paginationurl && $paginationallowall) {
-            // There are more than one page of results and we are in 'view all' mode, suggest to go back to paginated view mode.
-            $pagingbar = html_writer::tag('div', html_writer::link($paginationurl->out(false, array('perpage' => $CFG->coursesperpage)),
-                get_string('showperpage', '', $CFG->coursesperpage)), array('class' => 'paging paging-showperpage'));
-        }
-        // Display list of courses.
-        $attributes = $chelper->get_and_erase_attributes('courses');
-        $content = html_writer::start_tag('div', $attributes);
-        if (!empty($pagingbar)) {
-            $content .= $pagingbar;
-        }
-        $coursecount = 0;
-        // Renders each course that we want rendered.
-        foreach ($coursestorender as $course) {
-            $classes = ($coursecount % 2) ? 'odd' : 'even';
-            if ($coursecount == 1) {
-                $classes .= ' first';
-            }
-            if ($coursecount >= count($coursestorender)) {
-                $classes .= ' last';
-            }
-            $content .= $this->coursecat_coursebox($chelper, $course, $classes);
-            $coursecount += 1;
-        }
-        if (!empty($pagingbar)) {
-            $content .= $pagingbar;
-        }
-        if (!empty($morelink)) {
-            $content .= $morelink;
-        }
-        $content .= html_writer::end_tag('div'); // .courses
-        return $content;
-    }
 }
