2fb60ab73f8f738ba8009875059397464458a094
John Azinheira <jva357@gmail.com>
2021-10-01 14:25:04 -0400

    Version bump (2021090700)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021090600;
+$plugin->version   = 2021090700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires  = 2021051703;
 $plugin->component = 'theme_saylor';
