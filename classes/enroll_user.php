150c2e5ecd693ddc1eca593504408a4564c6175a
David Ta <david.ta@saylor.org>
2023-09-12 13:25:23 -0500

    Update enroll_user.php
    Removed required permission and added a parameter to specify the user to enroll.

--- classes/enroll_user.php
+++ classes/enroll_user.php
@@ -7,14 +7,13 @@ $courseid = required_param('courseid', PARAM_INT);
 require_login();
 
 $context = context_course::instance($courseid);
-require_capability('moodle/course:enrolreview', $context);
 
 $enrol = enrol_get_plugin('self');
 if ($enrol) {
     $instances = enrol_get_instances($courseid, true);
     foreach ($instances as $instance) {
         if ($instance->enrol === 'self') {
-            $enrol->enrol_self($instance);
+            $enrol->enrol_self($instance, $USER->id);
             break;
         }
     }
